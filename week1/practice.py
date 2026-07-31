eeac0e9051077bbbc9a12243a45b7bc62226eba3
hayleyselcraig <hayleyselcraig@live.com>
2026-07-31 20:55:27 +0100

    Complete Week 1 Python fundamentals

---
+++ week1/practice.py
@@ -0,0 +1,16 @@
+# TASK 1 - Print my name to the screen
+print("My name is Hayley.")
+
+# TASK 2 - Store age, favourite colour & favourite animal
+age = 27
+
+fav_colour = "purple"
+
+fav_animal = "red panda"
+
+# TASK 3 - Print these stored variables with a f string
+print(f"My name is Hayley and i'm {age}. My favourite colour is {fav_colour} and my favourite animal is a {fav_animal}.")
+
+# TASK 4 - Asks the user for their name and stores it in a variable.
+name = input("What is your name?")
+print(f"Hello, {name}!")
